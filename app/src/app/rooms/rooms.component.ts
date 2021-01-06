3d57c2eb40a18654cfd56d19f246b95d11d1847a
Eric Xue <ovo4096@gmail.com>
2021-01-06 17:21:00 +0800

    add room list link to room

--- app/src/app/rooms/rooms.component.ts
+++ app/src/app/rooms/rooms.component.ts
@@ -2,6 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {AlertController} from '@ionic/angular';
 import {I18nService} from '../service/i18n.service';
 import {LoggerService} from '../service/logger.service';
+import {AdminServer} from '../config';
+import {WlRoomInfo} from '../defines';
+import {WlhttpService} from '../service/wlhttp.service';
+import {ProfileService} from '../service/profile.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-rooms',
@@ -9,14 +14,20 @@ import {LoggerService} from '../service/logger.service';
   styleUrls: ['rooms.component.scss']
 })
 export class RoomsComponent implements OnInit {
+  public rooms: any = [];
+
   constructor(
     private alert: AlertController,
     public i18n: I18nService,
     private logger: LoggerService,
+    private http: WlhttpService,
+    private profile: ProfileService,
+    public router: Router,
   ) {
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.rooms = await this.http.http.get(`https://${AdminServer.address}/room/list`).toPromise();
   }
 
   async logout() {
@@ -43,5 +54,21 @@ export class RoomsComponent implements OnInit {
     });
 
     await alert.present();
+  }
+
+  gotoRoom(room: any) {
+    this.profile.roomId = room.id;
+    this.getRoomInfo(room.id);
+  }
+
+  getRoomInfo(roomid) {
+    const roomDetailUrl = `https://${AdminServer.address}/room/info/${roomid}`;
+    this.http.http.get(roomDetailUrl).toPromise().then(roomInfo => {
+      this.logger.debug('room info: ', roomInfo);
+      this.profile.roomInfo = roomInfo as WlRoomInfo;
+      this.router.navigateByUrl('/');
+    }).catch(error => {
+      this.logger.error(error.error);
+    });
   }
 }
